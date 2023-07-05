80d2761137f816693fdfe538470298812db1cc04
RianIhsan <Rianwhatsup@gmail.com>
2023-07-05 13:30:19 +0700

    Example CRUD Cloudinary

--- src/controllers/book/index.ts
+++ src/controllers/book/index.ts
@@ -1,13 +1,83 @@
-// import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  UploadedFile,
+  UseInterceptors,
+  Post,
+  Get,
+  Param,
+  Put,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Book } from 'src/entities/dto';
+import { CloudinaryService } from 'src/services';
+import { BookService } from 'src/services/book';
 
-// import { AppService } from './app.service';
+@Controller('books')
+export class BooksController {
+  constructor(
+    private cloudinaryService: CloudinaryService,
+    private booksService: BookService,
+  ) {}
 
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadBooks(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() book: Book,
+  ): Promise<Book> {
+    const imgaeUrl = await this.cloudinaryService.uploadImage(file);
+    const saveBook = await this.booksService.createBook({
+      title: book.title,
+      description: book.description,
+      author: book.author,
+      image: imgaeUrl,
+    });
+    return saveBook;
+  }
 
-//   @Get()
-//   getData() {
-//     return this.appService.getData();
-//   }
-// }
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.booksService.getAllBooks();
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id') id: number): Promise<Book> {
+    return this.booksService.getBookByID(id);
+  }
+
+  @Put(':id')
+  @UseInterceptors(FileInterceptor('image'))
+  async updateBook(
+    @Param('id') id: number,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() updatedBook: Book,
+  ): Promise<Book> {
+    let imageUrl = updatedBook.image;
+
+    if (file) {
+      const uploadedImage = await this.cloudinaryService.uploadImage(file);
+      imageUrl = uploadedImage.secure_url;
+    }
+
+    const book: Book = {
+      ...updatedBook,
+      image: imageUrl,
+    };
+
+    return this.booksService.updateBook(id, book);
+  }
+
+  @Delete(':id')
+  async deleteBookById(@Param('id') id: number): Promise<{ message: string }> {
+    const book = await this.booksService.getBookByID(id);
+    if (!book) {
+      throw new NotFoundException('Buku tidak ada');
+    }
+
+    await this.booksService.deleteBookById(id);
+    return { message: 'Berhasil hapus buku' };
+  }
+}
--- src/services/book/index.ts
+++ src/services/book/index.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma';
+import { Book } from 'src/entities/dto';
 
 @Injectable()
-export class AppService {
+export class BookService {
   constructor(private prisma: PrismaService) {}
-  async createBook(): Promise<{ message: string }> {
-    return { message: 'Hello API' };
+
+  async createBook(data: Book): Promise<Book> {
+    return this.prisma.books.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        author: data.author,
+        image: data.image.secure_url,
+      },
+    });
+  }
+
+  async getAllBooks(): Promise<Book[]> {
+    return this.prisma.books.findMany();
+  }
+
+  async getBookByID(id: number): Promise<Book> {
+    return this.prisma.books.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+  }
+
+  async updateBook(id: number, data: Book): Promise<Book> {
+    return this.prisma.books.update({
+      where: {
+        id: Number(id),
+      },
+      data,
+    });
+  }
+
+  async deleteBookById(id: number): Promise<void> {
+    await this.prisma.books.delete({
+      where: {
+        id: Number(id),
+      },
+    });
   }
 }
